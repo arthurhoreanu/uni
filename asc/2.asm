c7da57928aef5a7ed0afe3cb63971d9fd9f1f8b1
arthurhoreanu <arthur.horeanu@gmail.com>
2023-11-06 21:09:19 +0200

    add: 2.asm to asc

--- asc/2.asm
+++ asc/2.asm
@@ -1,16 +1,13 @@
 global main
 section .data
-m db ''
+m db 53
 section .text
 
 main:
 
-mov rax, 0
-mov rdi, 0
-mov rsi, m
-mov rdx, 1
-syscall
-
+mov al,[m]
+add al,'0'
+Mov [m],al
 
 mov rax, 1
 mov rdi, 1
