24779361a143b6c9a84fdebab0e715c03520b0d5
arthurhoreanu <arthur.horeanu@gmail.com>
2024-10-13 22:30:20 +0300

    MAP/L1: Added Aufgabe 4.2: x esc git add .

--- II.3/map/l/l1/src/Aufgabe4.java
+++ II.3/map/l/l1/src/Aufgabe4.java
@@ -2,9 +2,13 @@ public class Aufgabe4 {
     public static void main(String[] args) {
         Aufgabe4 aufgabe4 = new Aufgabe4();
         int[] keyboards = {40, 35, 70, 15, 45};
+        int[] keyboardsTwo = {15, 20, 10, 35};
+        int[] USBs = {20, 15, 40, 15};
 
         System.out.println("Aufgabe 4:");
         System.out.println("Cheapest keyboard: " + aufgabe4.cheapest(keyboards));
+        System.out.println("Most expensive keyboard: " + aufgabe4.mostExpensive(keyboardsTwo));
+        System.out.println("Most expensive USB: " + aufgabe4.mostExpensive(USBs));
         System.out.println("");
     }
 
@@ -14,6 +18,14 @@ public class Aufgabe4 {
             if (item < min)
                 min = item;
         return min;
+    }
+
+    public int mostExpensive(int[] items) {
+        int max = Integer.MIN_VALUE;
+        for (int item : items)
+            if(item > max)
+                max = item;
+        return max;
     }
 
 }
