be7db19e463fef6655a76a0c5a5155d4e07807c5
arthurhoreanu <arthur.horeanu@gmail.com>
2024-10-13 23:52:28 +0300

    Ended MAP/L1: Added Aufgabe 4.4

--- II.3/map/l/l1/src/Aufgabe4.java
+++ II.3/map/l/l1/src/Aufgabe4.java
@@ -5,13 +5,20 @@ public class Aufgabe4 {
         int[] keyboardsTwo = {15, 20, 10, 35};
         int[] usbs = {20, 15, 40, 15};
         int[] usbsTwo = {15, 45, 20};
+        int[] keyboardsThree = {40, 50, 60};
+        int[] usbsThree = {8, 12};
+        int[] keyboardsFour = {60};
+        int[] keyboardsFive = {40, 60};
+
 
         System.out.println("Aufgabe 4:");
         System.out.println("Cheapest keyboard: " + aufgabe4.cheapest(keyboards));
         System.out.println("Most expensive keyboard: " + aufgabe4.mostExpensive(keyboardsTwo));
         System.out.println("Most expensive USB: " + aufgabe4.mostExpensive(usbs));
-        System.out.println("Most expensive budget that I can buy: " + aufgabe4.mostExpensiveWithinBudget(usbsTwo, 30));
-        System.out.println("");
+        System.out.println("Most expensive within budget: " + aufgabe4.mostExpensiveWithinBudget(usbsTwo, 30));
+        System.out.println("Budget: 60, Markus spends: " +aufgabe4.withinBudget(keyboardsThree, usbsThree, 60));
+        System.out.println("Budget: 60, Markus spends: " +aufgabe4.withinBudget(keyboardsFour, usbsThree, 60));
+        System.out.println("Budget: 60, Markus spends: " +aufgabe4.withinBudget(keyboardsFive, usbsThree, 60));
     }
 
     public int cheapest(int[] items) {
@@ -35,6 +42,18 @@ public class Aufgabe4 {
         for (int item : items)
             if (item > max && item <= budget)
                 max = item;
+        return max;
+    }
+
+    public int withinBudget(int[] keyboards, int[] usbs, int budget){
+        int max = -1;
+        for(int keyboard : keyboards){
+            for(int usb: usbs) {
+                int total = keyboard + usb;
+                if(max < total && total <= budget)
+                    max = total;
+            }
+        }
         return max;
     }
 
