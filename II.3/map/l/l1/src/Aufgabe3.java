2c090bc5b227d40dc144e5736021b4f913cb7032
arthurhoreanu <arthur.horeanu@gmail.com>
2024-10-13 21:14:26 +0300

    MAP/L1: Added Aufgabe 3.2

--- II.3/map/l/l1/src/Aufgabe3.java
+++ II.3/map/l/l1/src/Aufgabe3.java
@@ -5,8 +5,11 @@ public class Aufgabe3 {
         Aufgabe3 aufgabe3 = new Aufgabe3();
         int[] firstNumber = {1, 3, 0, 0, 0, 0, 0, 0, 0};
         int[] secondNumber = {8, 7, 0, 0, 0, 0, 0, 0, 0};
+        int[] thirdNumber = {8, 3, 0, 0, 0, 0, 0, 0, 0};
+        int[] fourthNumber = {5, 4, 0, 0, 0, 0, 0, 0, 0};
 
         System.out.println("130000000 + 870000000 = " + Arrays.toString(aufgabe3.sum(firstNumber, secondNumber)));
+        System.out.println("830000000 - 540000000 " + Arrays.toString(aufgabe3.difference(thirdNumber, fourthNumber)));
     }
 
         public int[] sum ( int[] number, int[] otherNumber){
@@ -26,4 +29,19 @@ public class Aufgabe3 {
             }
             return result;
         }
+
+    public int[] difference(int[] number, int[] otherNumber) {
+        int borrow = 0;
+        int[] result = new int[number.length];
+        for (int i = number.length - 1; i >= 0; i--) {
+            int diff = number[i] - otherNumber[i] - borrow;
+            if (diff < 0) {
+                diff += 10;
+                borrow = 1;
+            } else
+                borrow = 0;
+            result[i] = diff;
+        }
+        return result;
+    }
 }
