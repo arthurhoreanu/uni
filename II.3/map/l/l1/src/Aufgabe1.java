fde39b4b2ec677aebcd629749f20a6a31c249d66
arthurhoreanu <arthur.horeanu@gmail.com>
2024-10-13 19:25:39 +0300

    MAP/L1: Added Aufgabe 1.4

--- II.3/map/l/l1/src/Aufgabe1.java
+++ II.3/map/l/l1/src/Aufgabe1.java
@@ -10,13 +10,14 @@ public class Aufgabe1 {
         System.out.println("1. Grades with insufficient score: " + Arrays.toString(insufficientScoreArray));
         System.out.println("2. Average of the grades: " + aufgabe1.average(array));
         System.out.println("3. Rounded grades: " + Arrays.toString(roundedArray));
+        System.out.println("4. Maximum rounded grade: " + aufgabe1.maxRounded(array));
     }
 
     public int[] insufficientScore(int[] gradesArray) {
         int[] result = new int[0];
         int size = 0;
-        for(int grade : gradesArray){
-            if(grade < 40){
+        for (int grade : gradesArray) {
+            if (grade < 40) {
                 int[] temporary = new int[size + 1];
                 for (int i = 0; i < size; i++) {
                     temporary[i] = result[i];
@@ -39,8 +40,8 @@ public class Aufgabe1 {
 
     public int[] rounded(int[] gradesArray) {
         int[] result = new int[gradesArray.length];
-        int i=0;
-        for(int grade : gradesArray){
+        int i = 0;
+        for (int grade : gradesArray) {
             if (grade >= 38) {
                 int nextFiveMultiple = ((grade / 5) + 1) * 5;
                 if (nextFiveMultiple - grade < 3)
@@ -49,5 +50,14 @@ public class Aufgabe1 {
             result[i++] = grade;
         }
         return result;
+    }
+
+    public int maxRounded(int[] gradesArray) {
+        int[] temporaryArray = rounded(gradesArray);
+        int max = 0;
+        for (int grade : temporaryArray)
+            if (grade > max)
+                max = grade;
+        return max;
     }
 }
