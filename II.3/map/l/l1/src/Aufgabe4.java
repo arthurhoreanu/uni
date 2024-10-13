4f36d1a500256ef67e3c311f3149797b362e6685
arthurhoreanu <arthur.horeanu@gmail.com>
2024-10-13 22:45:23 +0300

    MAP/L1: Added Aufgabe 4.3

--- II.3/map/l/l1/src/Aufgabe4.java
+++ II.3/map/l/l1/src/Aufgabe4.java
@@ -3,12 +3,14 @@ public class Aufgabe4 {
         Aufgabe4 aufgabe4 = new Aufgabe4();
         int[] keyboards = {40, 35, 70, 15, 45};
         int[] keyboardsTwo = {15, 20, 10, 35};
-        int[] USBs = {20, 15, 40, 15};
+        int[] usbs = {20, 15, 40, 15};
+        int[] usbsTwo = {15, 45, 20};
 
         System.out.println("Aufgabe 4:");
         System.out.println("Cheapest keyboard: " + aufgabe4.cheapest(keyboards));
         System.out.println("Most expensive keyboard: " + aufgabe4.mostExpensive(keyboardsTwo));
-        System.out.println("Most expensive USB: " + aufgabe4.mostExpensive(USBs));
+        System.out.println("Most expensive USB: " + aufgabe4.mostExpensive(usbs));
+        System.out.println("Most expensive budget that I can buy: " + aufgabe4.mostExpensiveWithinBudget(usbsTwo, 30));
         System.out.println("");
     }
 
@@ -24,6 +26,14 @@ public class Aufgabe4 {
         int max = Integer.MIN_VALUE;
         for (int item : items)
             if(item > max)
+                max = item;
+        return max;
+    }
+
+    public int mostExpensiveWithinBudget(int[] items, int budget) {
+        int max = 0;
+        for (int item : items)
+            if (item > max && item <= budget)
                 max = item;
         return max;
     }
