3ebb6b2624573d0bc7e09c93d6a91eec25505ca1
arthurhoreanu <arthur.horeanu@gmail.com>
2025-02-28 17:52:33 +0200

    update: II.4/ai/hmm

--- II.4/ai/s/hmm.py
+++ II.4/ai/s/hmm.py
@@ -1,30 +1,35 @@
 import numpy as np
 
 def predict(tm, px):
-    return np.dot(tm, px)
+    return tm @ px
 
 def update(om, px, obs):
-    likelihood = om[:, obs]
-    px_new = likelihood * px
-    return px_new / np.sum(px_new)
+    if obs == "w":
+        i = 0
+    else:
+        i = 1
+    px2 = np.multiply(px, om[:, i])
+    return px2 / np.sum(px2)
 
 def filter(tm, om, y1t, p0):
-    px = p0
-    for obs in y1t:
-        px = predict(tm, px)
-        px = update(om, px, 0 if obs == "w" else 1)
-    return px
+    pt = p0
+    pl = [pt]
+    for t in range(len(y1t)):
+        pt_pred = predict(tm, pt)
+        pt = update(om, pt_pred, y1t[t])
+        pl = pl + [pt]
+    return pl
 
 p0 = np.array([0.5, 0.5])
 
 tm = np.array([
-    [0.7, 0.3],
-    [0.2, 0.8]
+    [0.7, 0.2],
+    [0.3, 0.8]
 ])
 
 om = np.array([
     [0.95, 0.05],
-    [0.40, 0.60]
+    [0.4, 0.6]
 ])
 
 y1t = ["w", "w", "v", "v"]
